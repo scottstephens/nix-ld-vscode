5ade883b643fbf4eb862e849e86706d162e39f7c
Scott Stephens <stephens.js@gmail.com>
2022-12-06 04:58:06 +0000

    First working version

--- module.nix
+++ module.nix
@@ -7,6 +7,19 @@
   options = {
   };
 
-  config = {
+  config.home.file = {
+    vscode = {
+      target = ".vscode-server/server-env-setup"; 
+      text = 
+        let
+          nix_ld_library_path_val = pkgs.lib.makeLibraryPath [
+            pkgs.stdenv.cc.cc
+          ];
+          nix_ld_path_file = "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
+        in ''
+          export NIX_LD_LIBRARY_PATH=${nix_ld_library_path_val}
+          export NIX_LD=$(cat ${nix_ld_path_file})
+          '';
+    };
   };
 }
